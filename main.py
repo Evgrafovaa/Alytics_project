e9ebaf6e1dcda9b10025f72df71173987f458673
Evgrafovaa <42068556+Evgrafovaa@users.noreply.github.com>
2018-08-03 17:48:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#http://lybniz2.sourceforge.net/safeeval.html
+
+def eval_num(line, t):
+    res = eval(line)
+    return res
+
+
+def print_func(line, min_t, max_t, dt):
+    t = np.arange(min_t, max_t, dt)
+    f = [eval_num(line, n) for n in t]
+    plt.plot(t, f)
+    plt.xlabel('time (s)')
+    plt.ylabel('voltage (mV)')
+    plt.title('About as simple as it gets, folks')
+    plt.grid(True)
+    plt.show()
+
+def main():
+    print_func("t*t", 0, 10, 1)
+
+main()
